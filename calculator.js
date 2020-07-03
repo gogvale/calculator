53aabe112ad4dad9fe9049564a4f3f1a05f5e14a
Gabriel Vale <gogvale2011@gmail.com>
2020-07-02 20:57:37 -0500

    Add decimal point, add numbers

--- calculator.js
+++ calculator.js
@@ -1,4 +1,6 @@
 let currentNumber = 0,
+  fraction = 0.123,
+  hasFraction = false,
   oldNumber,
   operator;
 const precision = 10 ** 9;
@@ -6,6 +8,7 @@ const topDisplay = document.querySelector("#top");
 const bottomDisplay = document.querySelector("#bottom");
 
 function toDecimalFormat(number) {
+  if (number < 1 && number > 0) return ("" + number).split(".")[1];
   let fraction = "" + Math.round((number % 1) * precision) / precision;
   fraction = "." + fraction.split(".")[1];
 
@@ -24,9 +27,42 @@ function toDecimalFormat(number) {
   return number;
 }
 function updateDOM() {
-  topDisplay.textContent = oldNumber;
-  bottomDisplay.textContent = toDecimalFormat(currentNumber);
+  if (!oldNumber) oldNumber = "";
+  if (!operator) operator = "";
+  topDisplay.textContent = `${oldNumber} ${operator}`;
+
+  bottomDisplay.textContent = hasFraction
+    ? toDecimalFormat(currentNumber) + "." + toDecimalFormat(fraction)
+    : toDecimalFormat(currentNumber);
+}
+function addDigit(digit) {
+  console.log(digit);
+  digit = digit.target.textContent;
+  if (digit !== ".") {
+    digit = +digit;
+    let fraction = currentNumber % 1;
+    currentNumber = Math.floor(currentNumber);
+    currentNumber = currentNumber * 10 + digit + fraction;
+  } else {
+    hasFraction = true;
+  }
+
+  updateDOM();
+}
+function deleteDigit() {
+  if (currentNumber === 0) return;
+  currentNumber = "" + currentNumber;
+  currentNumber = currentNumber.split("");
+  currentNumber.pop();
+  currentNumber = currentNumber.join("");
+  updateDOM();
 }
 
-currentNumber = 0;
+function initialize() {
+  const numbers = document.querySelectorAll(".number");
+  numbers.forEach((n) => n.addEventListener("click", addDigit));
+}
+initialize();
+
+currentNumber = 1234;
 updateDOM();
